a9918464ba9980a091c32562192288c896617d4f
loic <loic.pierron@outlook.fr>
2025-05-15 11:33:18 +0200

    Réorganisation : déplacement du contenu de Le_Studio_Website à la racine

--- Le_Studio_Website/js/main.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger');
-const nav = document.getElementById('main-nav');
-
-burger.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
